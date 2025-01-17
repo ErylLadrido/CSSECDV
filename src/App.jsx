c384559e4955271df38c98d3165b3712f3c23665
Miguel <miguel_gonzales@dlsu.edu.ph>
2025-01-17 23:19:50 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-4xl text-sky-500 underline font-bold'>TAILWIND TEST</h1>
+    </div>
+  )
+}
+
+export default App
