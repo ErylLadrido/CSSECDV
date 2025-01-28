2c6255c7aa54db9507546bfd6c753edf195fffb6
Miguel <miguel_gonzales@dlsu.edu.ph>
2025-01-28 17:27:50 +0800

    setup basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
+
+// Routes
 import App from './App.tsx'
+import Login from './pages/Login.tsx';
+import Signup from './pages/Signup.tsx';
+import AdminPanel from './pages/AdminPanel.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/admin" element={<AdminPanel/>}/>
+    </Routes>
+  </BrowserRouter>,
 )
