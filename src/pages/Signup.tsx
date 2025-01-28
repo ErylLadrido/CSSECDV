1a27e084ab640c843e50eafb00147285e2f8a4cf
Miguel <miguel_gonzales@dlsu.edu.ph>
2025-01-28 18:51:50 +0800

    started on signup form

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className='w-screen h-screen bg-blue-300 flex justify-center items-center'>
+      <form
+        className='bg-blue-200 w-2/3 h-2/3'
+      >
+
+      </form>
+    </div>
   )
 }
 
