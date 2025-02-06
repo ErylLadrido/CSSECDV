4e9f6a57e8bc52baa07381ce574ddf0d36adac68
axvolkzki <amelia_abenoja@dlsu.edu.ph>
2025-02-06 23:35:55 +0800

    Changed: Signup -> Register

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 // Routes
 import Landing from './pages/Landing.tsx';
 import Login from './pages/Login.tsx';
-import Signup from './pages/Signup.tsx';
+import Signup from './pages/Register.tsx';
 import AdminPanel from './pages/AdminPanel.tsx';
 import UserPanel from './pages/UserPanel.tsx';
 import UserProfile from './pages/UserProfile.tsx';
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
             <Route path="/landing" element={<Landing />} />
-            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/register" element={<Signup/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/admin" element={<AdminPanel/>}/>
             <Route path="/" element={<UserPanel/>}/>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 type Props = {}
 
-export default function Signup({}: Props) {
+export default function Register({}: Props) {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
